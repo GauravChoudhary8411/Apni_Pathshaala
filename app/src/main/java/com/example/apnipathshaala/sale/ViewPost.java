47a366d2a6df6a83150994b2fdf1e87cfdec17d9
Gaurav Choudhary <gauravchoudhary.1191@gmail.com>
2021-05-04 13:24:15 +0530

    Optimizing the app

--- app/src/main/java/com/example/apnipathshaala/sale/ViewPost.java
+++ app/src/main/java/com/example/apnipathshaala/sale/ViewPost.java
@@ -4,11 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,13 +20,11 @@ import com.example.apnipathshaala.R;
 import com.example.apnipathshaala.models.Post;
 import com.example.apnipathshaala.utils.SquareImageView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.auth.User;
 import com.squareup.picasso.Picasso;
 
 public class ViewPost extends AppCompatActivity {
@@ -49,6 +50,8 @@ public class ViewPost extends AppCompatActivity {
         mcity = findViewById(R.id.post_city);
         mstate = findViewById(R.id.post_state);
         mcountry = findViewById(R.id.post_country);
+
+
         reference = FirebaseDatabase.getInstance().getReference().child("posts");
 
         String postkey = getIntent().getStringExtra("postkey");
